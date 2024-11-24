d09723a8a95010bd557b3b3420b7f08651d5c428
Jorge Filho <jorge.munizfilho@gmail.com>
2024-11-24 20:55:15 -0300

    Criando metodo para mudar de canal

--- sistema-smart-tv/src/SmartTv.java
+++ sistema-smart-tv/src/SmartTv.java
@@ -13,4 +13,25 @@ public class SmartTv {
         ligada = false;
     }
 
+    public void aumentarVolume() {
+        volume++;
+        System.out.println("Aumentando o volume para: " + volume);
+    }
+
+    public void diminuirVolume() {
+        volume--;
+        System.out.println("Diminuindo o volume para: " + volume);
+    }
+
+    public void aumentarCanal() {
+        canal++;
+    }
+
+    public void dimunuirCanal() {
+        canal--;
+    }
+
+    public void mudarCanal(int novoCanal) {
+        canal = novoCanal;
+    }
 }
--- sistema-smart-tv/src/Usuario.java
+++ sistema-smart-tv/src/Usuario.java
@@ -5,8 +5,20 @@ public class Usuario {
 
         SmartTv smartTv = new SmartTv();
 
-        System.out.println("TV Ligada? " + smartTv.ligada);
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+
+        smartTv.aumentarVolume();
+
         System.out.println("Canal atual: " + smartTv.canal);
+
+        smartTv.mudarCanal(13);
+        System.out.println("Canal atual: " + smartTv.canal);
+
+        System.out.println("Volume atual: " + smartTv.volume);
+
+        System.out.println("TV Ligada? " + smartTv.ligada);
         System.out.println("Volume atual: " + smartTv.volume);
 
         smartTv.ligar();
