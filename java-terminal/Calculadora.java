9c1c6fcb6819ff2b54bb0f95e0a5cab8ad75e28e
Jorge Filho <jorge.munizfilho@gmail.com>
2024-11-25 19:25:31 -0300

    Modificando arquivos

---
+++ java-terminal/Calculadora.java
@@ -0,0 +1,25 @@
+
+/**
+ * <h1>Calculadora</h1>
+ * A Calculadora realiza operações matemáticas entre números inteiros
+ * <p>
+ * <b>Note:</b> Leia atentamente a documentação destas classes para desfrutar
+ * dos recursos oferecidos pelo autor
+
+ *  *@author Jorge Filho
+ * @version 1.0
+ * @since 01/01/2024
+ */
+public class Calculadora {
+
+    /**
+     * Este método é utilizado para somar dois números inteiros
+     *
+     * @param numeroUm este é o primeiro parâmetro do método
+     * @param numeroDois este é o segundo parâmetro do método
+     * @return int o resultado deste método é a soma dos dois números.
+     */
+    public int somar(int numeroUm, int numeroDois) {
+        return numeroUm + numeroDois;
+    }
+}
